f0e1a3ba1b72c2226d518876c53179afb60f0cf9
lusenakalee <lusenakalee@gmail.com>
2023-04-12 21:54:28 +0300

    formatting server

--- rss-server/server.js
+++ rss-server/server.js
@@ -1,41 +1,32 @@
-import RSSParser from 'rss-parser';
-import cors from 'cors'
-import express from 'express'
+import RSSParser from "rss-parser";
+import cors from "cors";
+import express from "express";
 
-const feedURL ="https://african.business/sectors/economy/feed"; 
-const feedURL2 ="https://fingaz.co.zw/feed/"; 
-  
+const feedURL = "https://african.business/sectors/economy/feed";
+const feedURL2 = "https://fingaz.co.zw/feed/";
+
 const parser = new RSSParser();
-let articles=[];
+let articles = [];
 
-const parse = async url =>{
-    const feed = await parser.parseURL(url);
-   
+const parse = async (url) => {
+  const feed = await parser.parseURL(url);
 
-    
-    feed.items.forEach(item=>{
-       articles.push({ item  })
-       console.log(`${item.title}\n${item.link}`)
-    });
-   
-}
+  feed.items.forEach((item) => {
+    articles.push({ item });
+    console.log(`${item.title}\n${item.link}`);
+  });
+};
 
 parse(feedURL);
 parse(feedURL2);
 let app = express();
 app.use(cors());
-app.get('/',(req, res)=>{
-    res.send(articles);
-})
+app.get("/", (req, res) => {
+  res.send(articles);
+});
 
-
-
-
-
-
-const server = app.listen ("4000", () =>{
-     console.log("App will work")
-})
-
+const server = app.listen("4000", () => {
+  console.log("App will work");
+});
 
 export default server;
